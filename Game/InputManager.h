55ac549f0f743f84e1857477c57f37ab8dd2608f
OfirGilad <ofirgila@post.bgu.ac.il>
2024-10-13 23:24:57 +0300

    removed tabs

--- Game/InputManager.h
+++ Game/InputManager.h
@@ -3,84 +3,84 @@
 #include "game.h"
 
 
-	void mouse_callback(GLFWwindow* window,int button, int action, int mods)
-	{	
-		if(action == GLFW_PRESS )
-		{
-			Game *scn = (Game*)glfwGetWindowUserPointer(window);
-			double x2,y2;
-			glfwGetCursorPos(window,&x2,&y2);
-			scn->Picking((int)x2,(int)y2);
-		}
-	}
-	
-	void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+void mouse_callback(GLFWwindow* window,int button, int action, int mods)
+{	
+	if(action == GLFW_PRESS )
 	{
 		Game *scn = (Game*)glfwGetWindowUserPointer(window);
-		scn->MyTranslate(glm::vec3(0,0,xoffset),0);
+		double x2,y2;
+		glfwGetCursorPos(window,&x2,&y2);
+		scn->Picking((int)x2,(int)y2);
+	}
+}
+	
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	Game *scn = (Game*)glfwGetWindowUserPointer(window);
+	scn->MyTranslate(glm::vec3(0,0,xoffset),0);
 		
-	}
+}
 	
-	void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	Game *scn = (Game*)glfwGetWindowUserPointer(window);
+
+	if(action == GLFW_PRESS || action == GLFW_REPEAT)
 	{
-		Game *scn = (Game*)glfwGetWindowUserPointer(window);
-
-		if(action == GLFW_PRESS || action == GLFW_REPEAT)
+		switch (key)
 		{
-			switch (key)
-			{
-				case GLFW_KEY_ESCAPE:			
-					glfwSetWindowShouldClose(window,GLFW_TRUE);
+			case GLFW_KEY_ESCAPE:			
+				glfwSetWindowShouldClose(window,GLFW_TRUE);
+			break;
+			case GLFW_KEY_SPACE:
+				if(scn->IsActive())
+					scn->Deactivate();
+				else
+					scn->Activate();
+			break;
+
+			case GLFW_KEY_UP:
+				scn->MoveCamera(0,scn->zTranslate,0.4f);
 				break;
-				case GLFW_KEY_SPACE:
-					if(scn->IsActive())
-						scn->Deactivate();
-					else
-						scn->Activate();
+			case GLFW_KEY_DOWN:
+				//scn->shapeTransformation(scn->xGlobalRotate,-5.f);
+				//cout<< "down: "<<endl;
+				scn->MoveCamera(0,scn->zTranslate,-0.4f);
 				break;
 
-				case GLFW_KEY_UP:
-					scn->MoveCamera(0,scn->zTranslate,0.4f);
-					break;
-				case GLFW_KEY_DOWN:
-					//scn->shapeTransformation(scn->xGlobalRotate,-5.f);
-					//cout<< "down: "<<endl;
-					scn->MoveCamera(0,scn->zTranslate,-0.4f);
-					break;
-
-			default:
-				break;
-			}
+		default:
+			break;
 		}
 	}
+}
 	
-	void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	Game *scn = (Game*)glfwGetWindowUserPointer(window);
+
+	scn->UpdatePosition((float)xpos,(float)ypos);
+	if(glfwGetMouseButton(window,GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS)
 	{
-		Game *scn = (Game*)glfwGetWindowUserPointer(window);
-
-		scn->UpdatePosition((float)xpos,(float)ypos);
-		if(glfwGetMouseButton(window,GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS)
-		{
-			scn->MouseProccessing(GLFW_MOUSE_BUTTON_RIGHT);
-		}
-		else if(glfwGetMouseButton(window,GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
-		{
-			scn->MouseProccessing(GLFW_MOUSE_BUTTON_LEFT);
-		}
-
+		scn->MouseProccessing(GLFW_MOUSE_BUTTON_RIGHT);
+	}
+	else if(glfwGetMouseButton(window,GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+	{
+		scn->MouseProccessing(GLFW_MOUSE_BUTTON_LEFT);
 	}
 
-	void window_size_callback(GLFWwindow* window, int width, int height)
-	{
-		Game *scn = (Game*)glfwGetWindowUserPointer(window);
+}
 
-		scn->Resize(width,height);
+void window_size_callback(GLFWwindow* window, int width, int height)
+{
+	Game *scn = (Game*)glfwGetWindowUserPointer(window);
+
+	scn->Resize(width,height);
 		
-	}
+}
 
-	void Init(Display &display)
-	{
-		display.AddKeyCallBack(key_callback);
-		display.AddMouseCallBacks(mouse_callback,scroll_callback,cursor_position_callback);
-		display.AddResizeCallBack(window_size_callback);
-	}
+void Init(Display &display)
+{
+	display.AddKeyCallBack(key_callback);
+	display.AddMouseCallBacks(mouse_callback,scroll_callback,cursor_position_callback);
+	display.AddResizeCallBack(window_size_callback);
+}
